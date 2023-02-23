e0c49519077e52baf7a39e685b67978185e59f7e
David Zuniga <60298694+davezuniga@users.noreply.github.com>
2023-02-22 18:25:43 -0600

    Fix top space

---
+++ js/dark_mode.js
@@ -0,0 +1,9 @@
+
+let toggleButton = document.getElementById("dark-mode-toggle");
+
+toggleButton.addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+});
+
+
+// TODO: Create dark mode toggle and create CSS class
