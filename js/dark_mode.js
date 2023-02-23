9c926ae4052fd5684e74822fabddc79599f8e0ad
David Zuniga <60298694+davezuniga@users.noreply.github.com>
2023-02-22 18:57:39 -0600

    Delete css coments and dark mode js file

--- js/dark_mode.js
+++
@@ -1,9 +0,0 @@
-
-let toggleButton = document.getElementById("dark-mode-toggle");
-
-toggleButton.addEventListener("click", function() {
-  document.body.classList.toggle("dark-mode");
-});
-
-
-// TODO: Create dark mode toggle and create CSS class
